bf3c561ceedd18ccbeebc67d4e5f97d1bf887dd3
Akash35721 <2004akashchauhan@gmail.com>
2025-01-14 22:46:33 +0530

    Refactor and reorganize code by moving variable assignments and operations to a new file

---
+++ LAB 1/3.py
@@ -0,0 +1,8 @@
+name = input("Enter your name here: ")
+greet = f"Hello, {name}! Nice to meet you!"
+print(greet)
+
+dob = int(input("Enter your year of birth: "))
+current_year = 2024
+age = current_year - dob
+print(f"Currently your age is: {age}")
---
+++ LAB 1/4.py
@@ -0,0 +1,8 @@
+
+number = int(input("Enter the number you want to get checked: "))
+
+
+if number % 2 == 0:
+    print("The number given is even.")
+else:
+    print("The number given is odd.")

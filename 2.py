67bca609679691e2ec1fc0849d7824b57cc7aebc
Akash35721 <2004akashchauhan@gmail.com>
2025-01-14 22:32:55 +0530

    Implement variable assignments and basic operations for different data types

---
+++ 2.py
@@ -0,0 +1,37 @@
+# Q2. Create variables of different data types (integer, float, string, boolean) and perform basic
+# operations on them.
+# · Assign values to variables of different data types.
+interger=10
+float=50
+string="Hello "
+boolean=True
+
+
+addition=interger+float
+substraction=interger-float
+multiplication=interger*float
+division=interger/float
+modulus=interger%float
+
+print(f"Addition: {interger} + {float} = {addition}")
+print(f"Substraction: {interger} - {float} = {substraction}")
+print(f"Multiplication: {interger} * {float} = {multiplication}")
+print(f"Division: {interger} / {float} = {division}")
+print(f"Modulus: {interger} % {float} = {modulus}")
+
+
+
+#-------------------------------------------
+
+concatenated_string=string+"World !! "
+print(concatenated_string)
+
+
+
+andop=boolean and False
+orop=boolean or False
+notop= not boolean
+print(andop)
+print(orop)
+print(notop)
+

108e62b386d0ca8ec5784f74a247fe5b03a2b534
Akash35721 <2004akashchauhan@gmail.com>
2025-01-14 10:19:23 +0530

    Add basic arithmetic operations and print results

--- LAB 1/1.py
+++ LAB 1/1.py
@@ -1,1 +1,17 @@
-#
+
+num1 = 10
+num2 = 2
+
+
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+
+
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Substraction: {num1} - {num2} = {substraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+print(f"Division: {num1} / {num2} = {division}")
+print(f"Modulus: {num1} % {num2} = {modulus}")
